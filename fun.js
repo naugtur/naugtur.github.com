82df8d01829804c9ef161d4a309d3495c8186580
Naugtur <naugtur@gmail.com>
2013-10-28 23:11:14 +0100

    new linkblog, hosted on github

--- fun.js
+++ fun.js
@@ -1,35 +1,51 @@
-(function(D) {
+(function (D) {
 
-  function recur() {
-    var elements, element, current, sections = D.querySelectorAll('section');
-    for (var i = 0, li = sections.length; i < li; i += 1) {
-      sections[i].id='THIS';
-      elements = document.querySelectorAll('#THIS>*');
-      sections[i].id='';
-      current = sections[i];
-      for (var e = 0, le = elements.length; e < le; e += 1) {
-        element = document.createElement('div');
-        element.appendChild(elements[e]);
-        current.appendChild(element);
-        current = element;
-      }
+    function recur() {
+        var elements, element, current,
+            sections = D.getElementsByTagName('section');
+        for (var i = 0, li = sections.length; i < li; i += 1) {
+            current = sections[i];
+            elements = [].slice.apply(current.getElementsByTagName('*'));
+            for (var e = 0, le = elements.length; e < le; e += 1) {
+                if(elements[e].parentNode===sections[i]){
+                element = document.createElement('div');
+                element.appendChild(elements[e]);
+                current.appendChild(element);
+                current = element;
+                }
+            }
+        }
     }
-  }
+
+    window.blag = function (d) {
+        var row, 
+            i=0,
+            datarow=null,
+            target = D.getElementById('blag'),
+            data = d.data;
+        if (target) {
+            while(i<7 && data.length){
+                datarow=data.shift();
+                if(datarow.user.login==='naugtur'){
+                row = document.createElement('div');
+                row.innerHTML = datarow.body.replace(/\[([^\]]+)\]/g,'<a href="$1">$1</a>');
+                target.appendChild(row);
+                target = row;
+                i++
+                }
+            }
 
 
-  var data=window.blag,row,target=D.querySelectorAll('.blag')[0];
-  if(target){
-    for (var i = 0, l = Math.min(10,data.length); i < l; i += 1) {
-      row=document.createElement('div');
-      row.innerHTML=data[i].title +'<br>'+ data[i].text;
-      target.appendChild(row);
-      target=row;
+        }
     }
-    
-    
-  }
+
+        var sc = D.createElement('script');
+        sc.src = 'https://api.github.com/repos/naugtur/naugtur.github.com/issues/1/comments?callback=blag&page=1&per_page=10&sort=updated&direction=desc';
+        D.head.appendChild(sc);
 
 
-  recur();
 
-})(document)
+
+        recur();
+
+    })(document)
