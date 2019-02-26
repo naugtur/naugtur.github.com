fd41010213bb88fa49dcbb77e107f87c4dad5c2e
Naugtur <naugtur@gmail.com>
2019-02-26 14:12:50 +0100

    clean up main page

--- fun.js
+++ fun.js
@@ -17,35 +17,6 @@
         }
     }
 
-    window.blag = function (d) {
-        var row, 
-            i=0,
-            datarow=null,
-            target = D.getElementById('blag'),
-            data = d.data;
-        if (target) {
-            while(i<7 && data.length){
-                datarow=data.shift();
-                if(datarow.user.login==='naugtur'){
-                row = document.createElement('div');
-                row.innerHTML = datarow.body.replace(/\[([^\]]+)\]/g,'<a href="$1">$1</a>');
-                target.appendChild(row);
-                target = row;
-                i++
-                }
-            }
-
-
-        }
-    }
-
-        var sc = D.createElement('script');
-        sc.src = 'https://api.github.com/repos/naugtur/naugtur.github.com/issues/comments?callback=blag&page=1&per_page=10&sort=updated&direction=desc';
-        D.head.appendChild(sc);
-
-
-
-
         recur();
 
     })(document)
